3a8a646b4016532d31cd14aee86e48f1f3a43e37
Thuan <thuanvh@gmail.com>
2024-02-29 23:28:28 +0700

    update

--- fast.py
+++ fast.py
@@ -1,7 +1,15 @@
 from langchain_community.embeddings import LlamaCppEmbeddings
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("data.csv")
+print(df)
 
 llama = LlamaCppEmbeddings(model_path="ggml-vistral-7B-chat-q4_0.gguf")
 
-doc_result = llama.embed_documents([text])
+doc_result = llama.embed_documents(df["text"])
 print(doc_result)
+
+with open('output.npy','wb') as f:
+    np.save(f, doc_result)
 
