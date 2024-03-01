e2f685e6094457bed400fafc21dd9afbf6eca537
Thuan <thuanvh@gmail.com>
2024-03-01 23:28:15 +0700

    update

--- main.py
+++ main.py
@@ -34,8 +34,9 @@ def read_root():
 
 @app.post("/chat")
 def chat(item: Item):
-    query = "Xin chào"
-    query = "Tỉ số trận Manchester và Nottingham"
+    # query = "Xin chào"
+    # query = "Tỉ số trận Manchester và Nottingham"
+    query = item.message
     query_embed = llama.embed_query(query)
     print(query_embed)
 
@@ -46,9 +47,29 @@ def chat(item: Item):
     b = np.array(query_embed)
 
     scores = doc_data[:] @ b[:].T
-    print(scores)
+    #print(scores)
     max_index = np.argmax(scores)
-    print(max_index, scores[max_index])
+    #print(max_index, scores[max_index])
 
-    print(df['text'][max_index],df['action'][max_index])
-    return {"message": item.message, "text":df['text'][max_index], "action": df['action'][max_index]}
+    #print(df['text'][max_index],df['action'][max_index])
+    reply = score_specific(item.message)
+    return {"message": item.message, "similar_text":df['text'][max_index],"similarity":scores[max_index], "action": df['action'][max_index], "reply":reply}
+
+def score_specific(message : str):
+    # Example: reuse your existing OpenAI setup
+    from openai import OpenAI
+
+    # Point to the local server
+    client = OpenAI(base_url="http://192.168.111.1:1234/v1", api_key="not-needed")
+
+    completion = client.chat.completions.create(
+    model="local-model", # this field is currently unused
+    messages=[
+        {"role": "system", "content": "Hãy tổng hợp thông tin về tên hai đội bóng đá từ người dùng và chỉ trả lời dạng json ```json {""team1"":  Tên đội bóng đá 1, ""team2"": Tên đội bóng đá 2}. Hãy để trống tên đội bóng nếu không có thông tin"},
+        {"role": "user", "content": message}
+    ],
+    temperature=0.7,
+    )
+
+    print(completion.choices[0].message)
+    return completion.choices[0].message
